b7e91afa6697e8a316ad6e4ff8c78900ccb8fd79
IamThiago-IT <thiagodossantos315@gmail.com>
2024-04-01 17:13:29 -0300

    Config Inicial Backend

---
+++ src/test/java/com/ufpr/bantadsbackend/BantadsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufpr.bantadsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BantadsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
